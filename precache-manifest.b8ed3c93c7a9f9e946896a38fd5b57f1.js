3b144af0c4d8e7c939681e55d22f8decaf05fd73
brezich <brezich@yahoo.com>
2019-09-23 05:55:14 +0200

    Updates

---
+++ precache-manifest.b8ed3c93c7a9f9e946896a38fd5b57f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75db39628cc117eb5e1a9e93a721d151",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "dc06bd1197bb31354e1b",
+    "url": "/ravenous/static/css/main.7db109dc.chunk.css"
+  },
+  {
+    "revision": "a363e90da340cd32112b",
+    "url": "/ravenous/static/js/2.0bf020ca.chunk.js"
+  },
+  {
+    "revision": "dc06bd1197bb31354e1b",
+    "url": "/ravenous/static/js/main.adc0ac79.chunk.js"
+  },
+  {
+    "revision": "aaf3da94991164aa06e7",
+    "url": "/ravenous/static/js/runtime~main.ba68e623.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
